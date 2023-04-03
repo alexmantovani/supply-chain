6d4707966fb671857e9b874d42fe26281bf4b2a6
Alex Mantovani <mantovani.alex@gmail.com>
2023-04-03 19:27:09 +0200

    Dashbord per avere un menu sui cell

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,10 +8,19 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                <div class="flex justify-between p-6 text-gray-900 dark:text-gray-100">
+                    <x-nav-link :href="route('stock.index')" :active="request()->routeIs('stock.index')">
+                        {{ __('Magazzino') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('refill.index')" :active="request()->routeIs('refill.index')">
+                        {{ __('In esaurimento') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('order.index')" :active="request()->routeIs('order.index')">
+                        {{ __('Ordini') }}
+                    </x-nav-link>
                 </div>
             </div>
         </div>
+    </div>
     </div>
 </x-app-layout>
