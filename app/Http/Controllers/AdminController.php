c366fdd71b48145c237573fe1da5593d8c5fc09f
Alex Mantovani <mantovani.alex@gmail.com>
2023-10-07 07:40:27 +0200

    Mostrato nel grafico in Admin lo storico degli ordini chiusi (mostrare quelli aperti non ha senso)

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,7 +12,7 @@ class AdminController extends Controller
     {
         $graphOrders = Order::getOrdersDoneByYear();
 
-        $orders = Order::whereIn('status', ['waiting', 'pending'])
+        $orders = Order::whereIn('status', ['completed', 'closed'])
             ->join('warehouses', 'orders.warehouse_id', '=', 'warehouses.id')
             ->groupBy('warehouse_id')
             ->selectRaw('*, COUNT(*) as in_progress, warehouses.name as warehouse_name')
